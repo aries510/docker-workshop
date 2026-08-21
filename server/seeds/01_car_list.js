e4b47b38640f19d1a2b557527ddb6546c943f8b6
aries510 <darenrespicio@gmail.com>
2026-08-21 15:18:30 -0600

    still working on seeding table. left off at /server/seeds/01_car_list.js

--- server/seeds/01_car_list.js
+++ server/seeds/01_car_list.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('table_name').del()
   await knex('table_name').insert([
-    {manufacturer: 'Nissan', name: 'Fairlady Z (350Z)', chassis_code: 'Z33', years_manufactured: '2002 - 2008', engine: '3.5L V6 (VQ35)', drivetrain: 'Rear-Wheel Drive (RWD)', horsepower: 287, image_url: '/images/350z.jpg' },
-    {manufacturer: 'Nissan', name: 'value', chassis_code: 'value', years_manufactured: 'value', engine: 'value', drivetrain: ' value', horsepower: '123', image_url: 'url' },
+    {manufacturer: 'Nissan', name: 'Fairlady Z (350Z)', chassis_code: 'Z33', years_manufactured: '2002-2008', engine: '3.5L V6 (VQ35)', drivetrain: 'Rear-Wheel Drive (RWD)', horsepower: 287, image_url: '/images/350z.jpg' },
+    {manufacturer: 'Nissan', name: 'Silvia Spec-R', chassis_code: 'S15', years_manufactured: '1999-2002', engine: '2.0L I4 (SR20)', drivetrain: 'Rear-Wheel Drive (RWD)', horsepower: 250, image_url: '/images/silvia-spec-r.jpg' },
     {manufacturer: 'Nissan', name: 'rowValue3', chassis_code: 'value', years_manufactured: 'value', engine: 'value', drivetrain: ' value', horsepower: '123', image_url: 'url' },
     {manufacturer: 'Nissan', name: 'rowValue3', chassis_code: 'value', years_manufactured: 'value', engine: 'value', drivetrain: ' value', horsepower: '123', image_url: 'url' }
   ]);
