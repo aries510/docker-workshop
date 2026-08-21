3e0a78555af95379fb012ef19030f2fbe8e873c4
aries510 <darenrespicio@gmail.com>
2026-08-21 15:08:02 -0600

    working on seeding my table

---
+++ server/seeds/01_car_list.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('table_name').del()
+  await knex('table_name').insert([
+    {manufacturer: 'Nissan', name: 'Fairlady Z (350Z)', chassis_code: 'Z33', years_manufactured: '2002 - 2008', engine: '3.5L V6 (VQ35)', drivetrain: 'Rear-Wheel Drive (RWD)', horsepower: 287, image_url: '/images/350z.jpg' },
+    {manufacturer: 'Nissan', name: 'value', chassis_code: 'value', years_manufactured: 'value', engine: 'value', drivetrain: ' value', horsepower: '123', image_url: 'url' },
+    {manufacturer: 'Nissan', name: 'rowValue3', chassis_code: 'value', years_manufactured: 'value', engine: 'value', drivetrain: ' value', horsepower: '123', image_url: 'url' },
+    {manufacturer: 'Nissan', name: 'rowValue3', chassis_code: 'value', years_manufactured: 'value', engine: 'value', drivetrain: ' value', horsepower: '123', image_url: 'url' }
+  ]);
+};
